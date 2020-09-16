523b69910f77a55ec3a9e933435088f91d37bc0f
kwizl <karis.njoroge@gmail.com>
2020-09-16 18:26:45 +0300

    Ensure capitalize function passes test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const Functions = () => {
   const capitalize = (string) => {
-    
+    string = string.split('');
+    const str = string[0].toUpperCase();
+    string[0] = str;
+    return string.join('');
   };
 
   return { capitalize };
