dbd784cc5bbb393e8b51c19f18d797fcd109b29d
kwizl <karis.njoroge@gmail.com>
2020-09-16 19:19:24 +0300

    Group the calculator tests into describe block

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -1,20 +1,21 @@
 import Calculator from '../modules/calculator';
 
-const calculator = Calculator();
+describe('Calculator function tests', () => {
+  const calculator = Calculator();
 
-test('Calculator Add operation', () => {
-  expect(calculator.add(2, 4)).toBe(6);
-});
+  test('Calculator Add operation', () => {
+    expect(calculator.add(2, 4)).toBe(6);
+  });
 
-test('Calculator Subtract operation', () => {
-  expect(calculator.subtract(7, 2)).toBe(5);
-});
+  test('Calculator Subtract operation', () => {
+    expect(calculator.subtract(7, 2)).toBe(5);
+  });
 
-test('Calculator Divide operation', () => {
-  expect(calculator.divide(12, 4)).toBe(3);
-});
+  test('Calculator Divide operation', () => {
+    expect(calculator.divide(12, 4)).toBe(3);
+  });
 
-
-test('Calculator Multiply operation', () => {
-  expect(calculator.multiply(8, 3)).toBe(24);
+  test('Calculator Multiply operation', () => {
+    expect(calculator.multiply(8, 3)).toBe(24);
+  });
 });
