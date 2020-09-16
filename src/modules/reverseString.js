c19bcf7f287bd6a0ffcb1642cce5be96b3cb2f98
rsnazario <rafael.s.nazario@gmail.com>
2020-09-16 13:42:00 -0300

    TDD Method --> Function ReverseString that passes tests

--- src/modules/reverseString.js
+++ src/modules/reverseString.js
@@ -1,7 +1,15 @@
 const reverseString = () => {
   return {
     revertString: (string) => {
-      return 'hey';
+      if (string === '') {
+        return string;
+      } else {
+        let reverted = '';
+        for (let i = string.length - 1; i >= 0; i -= 1) {
+          reverted = reverted.concat(string[i]);
+        }
+        return reverted;
+      }
     }
   }
 };
