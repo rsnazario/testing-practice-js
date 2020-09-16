e5d9a5b0ea97acda89f265154796511b0bdbbfe1
kwizl <karis.njoroge@gmail.com>
2020-09-16 21:11:05 +0300

    Add more TDD test for caesar cipher method

--- src/test/caesarCipher.test.js
+++ src/test/caesarCipher.test.js
@@ -6,4 +6,16 @@ describe('Testing Caesar Cipher', () => {
   test('test normal string', () => {
     expect(cipher.caesar('abc', 3)).toBe('def');
   });
+
+  test('test keeping the same case', () => {
+    expect(cipher.caesar('Abc', 3)).toBe('def');
+  });
+
+  test('test if character is string', () => {
+    expect(cipher.caesar('"&%932!"', 3)).toBeFalsy();
+  });
+
+  test('test if there is a space in string', () => {
+    expect(cipher.caesar('a ', 3)).toBeFalsy();
+  });
 });
