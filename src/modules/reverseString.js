66108d7430d5fbc7a9385956360e7f90b6a1e24a
rsnazario <rafael.s.nazario@gmail.com>
2020-09-16 15:01:09 -0300

    Linter Fix

--- src/modules/reverseString.js
+++ src/modules/reverseString.js
@@ -1,17 +1,14 @@
-const reverseString = () => {
-  return {
-    revertString: (string) => {
-      if (string === '') {
-        return string;
-      } else {
-        let reverted = '';
-        for (let i = string.length - 1; i >= 0; i -= 1) {
-          reverted = reverted.concat(string[i]);
-        }
-        return reverted;
-      }
+const reverseString = () => ({
+  revertString: (string) => {
+    if (string === '') {
+      return string;
     }
-  }
-};
+    let reverted = '';
+    for (let i = string.length - 1; i >= 0; i -= 1) {
+      reverted = reverted.concat(string[i]);
+    }
+    return reverted;
+  },
+});
 
 export default reverseString;
--- src/test/arrayAnalyze.test.js
+++ src/test/arrayAnalyze.test.js
@@ -5,21 +5,21 @@ const testingArray = [3, 10, 1, 5, 7];
 
 test('Returns an Object', () => {
   const result = analysis.analyze([1, 2, 3]);
-  expect( typeof result ).toBe('object');
+  expect(typeof result).toBe('object');
 });
 
 test('Returns min value', () => {
-  expect( analysis.analyze(testingArray).min).toBe(1);
+  expect(analysis.analyze(testingArray).min).toBe(1);
 });
 
 test('Returns max value', () => {
-  expect( analysis.analyze(testingArray).max).toBe(10);
+  expect(analysis.analyze(testingArray).max).toBe(10);
 });
 
 test('Returns Average Value (natural rounded part only)', () => {
-  expect( analysis.analyze(testingArray).average).toBe(5);
+  expect(analysis.analyze(testingArray).average).toBe(5);
 });
 
 test('Returns Array Length', () => {
-  expect( analysis.analyze(testingArray).length).toBe(5);
+  expect(analysis.analyze(testingArray).length).toBe(5);
 });
